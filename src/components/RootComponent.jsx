c7bc678877c43d919b78326bff892d9e5695a76f
marcusaureliusg <marcusfgreen@gmail.com>
2024-02-18 23:46:00 -0700

    RootComponent updates, new dynamic state, new command input update

--- src/components/RootComponent.jsx
+++ src/components/RootComponent.jsx
@@ -9,11 +9,24 @@ import Modal from "./Modal";
 const RootComponent = () => {
   const [useCustomMinutes, setUseCustomMinutes] = useState(false);
   const [customMinutes, setCustomMinutes] = useState(null);
+  const [modalContentSrc, setModalContentSrc] = useState("");
+  const [modalContentClass, setModalContentClass] = useState("");
+  const [modalBGClass, setModalBGClass] = useState("");
   const [showModal, setShowModal] = useState(false);
   const timerBeepAudio = useRef(new Audio(timerBeep));
   const fourMinAudio = useRef(null);
 
-  // Initialize audioRef.current on component mount
+  const commonIframeProps = {
+    width: "560",
+    height: "315",
+    frameBorder: "0",
+    allow:
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+    allowFullScreen: true,
+    title: "YouTube video player",
+  };
+
+  // Initialize fourMinAudio.current on component mount
   useEffect(() => {
     fourMinAudio.current = new Audio(fourMin);
   }, []);
@@ -38,22 +51,36 @@ const RootComponent = () => {
   // Close modal and reset audio
   const closeModal = () => {
     setShowModal(false);
+    setModalContentSrc("");
+    setModalContentClass("");
+    setModalBGClass("");
   };
 
   const inputHandler = (command) => {
     timerBeepAudio.current.play();
     console.log(command);
     // If the command matches, set customMinutes and useCustomMinutes to trigger the countdown
-    if (command === "4 8 15 16 23 42") {
+    if (command.trim() === "4 8 15 16 23 42") {
       timerBeepAudio.current.play();
+      setModalContentClass("modal-content-slim");
+      setModalBGClass("modal-backdrop-slim");
+      setModalContentSrc(
+        "https://www.youtube.com/embed/XMR7JjgqsjE?si=Ke6KFr5l4apdi5XC&autoplay=1"
+      );
       console.log("Abort Sequence");
       setCustomMinutes(108);
       setUseCustomMinutes(true);
       stopAudio();
       if (!showModal) setShowModal(true);
     }
-    if (command === "7946 0893") {
+    if (command.replace(/\s+/g, "") === "79460893") {
       console.log("I've always loved you");
+      setModalContentClass("modal-content-eng");
+      setModalBGClass("modal-backdrop-eng");
+      setModalContentSrc(
+        "https://www.youtube.com/embed/iUaHMOWRYpI?si=ZxW_tbDNGkrM0UI0&autoplay=1"
+      );
+      if (!showModal) setShowModal(true);
     }
     if (command === "001") {
       timerBeepAudio.current.play();
@@ -69,19 +96,10 @@ const RootComponent = () => {
       <Modal
         showModal={showModal}
         onClose={closeModal}
-        contentClass="modal-content-slim"
-        classes="modal-backdrop-slim"
+        contentClass={modalContentClass}
+        classes={modalBGClass}
       >
-        <iframe
-          id="orientation"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/XMR7JjgqsjE?si=Ke6KFr5l4apdi5XC&autoplay=1"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+        <iframe {...commonIframeProps} src={modalContentSrc}></iframe>
       </Modal>
       <footer className="footer-container">
         <p className="footer-text">Built with ♥ by us</p>
