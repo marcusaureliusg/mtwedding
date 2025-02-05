4beeab7eee8d62c1f02e351f98958121753b3677
marcusaureliusg <marcusfgreen@gmail.com>
2025-02-05 12:01:14 -0700

    Optimizations
    - Add react compiler
    - Reduce asset sizes

--- src/assets/hmoon/cph/index.js
+++ src/assets/hmoon/cph/index.js
@@ -25,8 +25,53 @@ import c24 from "./cwhl (24).jpg";
 import c25 from "./cwhl (25).jpg";
 import c26 from "./cwhl (26).jpg";
 import c27 from "./cwhl (27).jpg";
+import c28 from "./cwhl (28).jpg";
+import c29 from "./cwhl (29).jpg";
+import c30 from "./cwhl (30).jpg";
+import c31 from "./cwhl (31).jpg";
+import c32 from "./cwhl (32).jpg";
+import c33 from "./cwhl (33).jpg";
+import c34 from "./cwhl (34).jpg";
+import c35 from "./cwhl (35).jpg";
+import c36 from "./cwhl (36).jpg";
+import c37 from "./cwhl (37).jpg";
 
 export const cwhlImages = [
-  c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15,
-  c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27
+  c1,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  c8,
+  c9,
+  c10,
+  c11,
+  c12,
+  c13,
+  c14,
+  c15,
+  c16,
+  c17,
+  c18,
+  c19,
+  c20,
+  c21,
+  c22,
+  c23,
+  c24,
+  c25,
+  c26,
+  c27,
+  c28,
+  c29,
+  c30,
+  c31,
+  c32,
+  c33,
+  c34,
+  c35,
+  c36,
+  c37,
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const ReactCompilerConfig = {
+  target: "18", // '17' | '18' | '19'
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
+});
